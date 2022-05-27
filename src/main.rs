edf474519f6bfcc71565d358a81e70dab4609376
Jefferson Quesado <jeff.quesado@gmail.com>
2022-05-27 14:50:22 -0300

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn get_topmost_int(stack: &mut [u8], stack_size: &mut usize) -> u32 {
     let base_stack = *stack_size - 4;
     *stack_size = base_stack;
-    
-    (stack[base_stack] as u32) * 0x1000000 + (stack[base_stack + 1] as u32) * 0x10000 +
-            (stack[base_stack + 2] as u32) *0x100 + (stack[base_stack + 3] as u32)
+
+    (stack[base_stack] as u32) * 0x1000000
+        + (stack[base_stack + 1] as u32) * 0x10000
+        + (stack[base_stack + 2] as u32) * 0x100
+        + (stack[base_stack + 3] as u32)
 }
 
 fn push_int(stack: &mut [u8], data_to_push: u32, stack_size: &mut usize) {
@@ -18,12 +20,14 @@ fn bytecode_bipush(stack: &mut [u8], data_to_push: u8, stack_size: &mut usize) {
     push_int(stack, data_to_push.into(), stack_size);
 }
 
-fn binary_int_operation<Op>(stack: &mut [u8], stack_size: &mut usize, op: Op) -> u32 
-        where
-        Op: Fn(u32, u32) -> u32 {
-    
-
-    let result = op(get_topmost_int(stack, stack_size), get_topmost_int(stack, stack_size));
+fn binary_int_operation<Op>(stack: &mut [u8], stack_size: &mut usize, op: Op) -> u32
+where
+    Op: Fn(u32, u32) -> u32,
+{
+    let result = op(
+        get_topmost_int(stack, stack_size),
+        get_topmost_int(stack, stack_size),
+    );
     push_int(stack, result, stack_size);
     result
 }
@@ -47,11 +51,20 @@ pub(crate) fn main() {
     bytecode_bipush(&mut stack, 255, &mut stack_size);
     bytecode_bipush(&mut stack, 2, &mut stack_size);
 
-    println!("[0] {} [1] {} [2] {} [3] {}", stack[0], stack[1], stack[2], stack[3]);
+    println!(
+        "[0] {} [1] {} [2] {} [3] {}",
+        stack[0], stack[1], stack[2], stack[3]
+    );
     bytecode_iadd(&mut stack, &mut stack_size);
-    println!("[0] {} [1] {} [2] {} [3] {}", stack[0], stack[1], stack[2], stack[3]);
+    println!(
+        "[0] {} [1] {} [2] {} [3] {}",
+        stack[0], stack[1], stack[2], stack[3]
+    );
     let sum = bytecode_iadd(&mut stack, &mut stack_size);
-    println!("[0] {} [1] {} [2] {} [3] {}", stack[0], stack[1], stack[2], stack[3]);
+    println!(
+        "[0] {} [1] {} [2] {} [3] {}",
+        stack[0], stack[1], stack[2], stack[3]
+    );
     println!("soma {}", sum);
     bytecode_bipush(&mut stack, 2, &mut stack_size);
     let mul = bytecode_imul(&mut stack, &mut stack_size);
